b24210153572a2ba8c21d5a76563b3259c324c24
Mikhaill96 <161833978+Mikhaill96@users.noreply.github.com>
2024-03-03 17:57:07 +0300

    1

---
+++ checkSumSign.java
@@ -0,0 +1,17 @@
+// 2 задание
+public class checkSumSign {
+    public static void main(String[] args) {
+        int a = -3;
+        int b = -1;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
+
