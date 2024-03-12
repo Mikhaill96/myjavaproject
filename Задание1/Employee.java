0f979f4f4cf2dd8f5a6eb74cbc6832d64ef33092
Mikhaill96 <161833978+Mikhaill96@users.noreply.github.com>
2024-03-12 16:29:03 +0300

    1

---
+++ Задание1/Employee.java
@@ -0,0 +1,43 @@
+package Задание1;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(
+                "Иванов Иван Иванович",
+                "Разработчик",
+                "ivanov@example.com",
+                "+79991234567",
+                100000,
+                30
+        );
+        employee1.displayInfo();
+    }
+}
